3986b92451ee2d1a89ad4ffb1bc478a3905d3c4e
Mario Carande Scarafia <mdc.mariio@gmail.com>
2023-06-16 00:53:07 -0300

    Add connection with api

---
+++ src/api/Api.ts
@@ -0,0 +1,17 @@
+import axios, { AxiosResponse } from 'axios';
+
+const apiKey = 'efbc2b95033e7dde757b6c455744baa2';
+
+export const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: apiKey,
+  }
+});
+
+export const authenticate = async () => {
+    await api.get('/authentication')
+    .catch((error: Error) => {
+      console.error('Error al validar la clave de API:', error.message);
+    });
+  }
