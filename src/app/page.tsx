6169a9504032fcba2487b31014a8f17a3b22e048
Mario Carande Scarafia <mdc.mariio@gmail.com>
2023-06-26 11:00:40 -0300

    Update minor change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,9 @@ const App = () => (
   <main className="flex min-h-screen flex-col items-center p-24 bg-gradient-to-r from-slate-900 to-slate-700">
     <section>
       <div>
-        <h1 className="text-5xl font-bold text-white">Movies App</h1>
+        <h1 className="text-center text-5xl font-bold text-white">
+          Movies App
+        </h1>
       </div>
     </section>
 
